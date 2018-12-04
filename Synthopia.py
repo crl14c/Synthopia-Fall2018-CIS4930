b5a43d95cf819301a6863548a1d8eb3fa14108fd
ars15b <ars15b@my.fsu.edu>
2018-12-03 19:35:13 -0500

    buttons are clickable and send you to their functions to do stuff and the exit button does exit the program

--- Synthopia.py
+++ Synthopia.py
@@ -12,6 +12,7 @@ white = (255, 255, 255)
 purple =(128, 0, 128)
 green = (0, 255, 0)
 grey = (128, 128, 128)
+lightblue = (173, 216, 230)
 
 
 gameDisplay = pygame.display.set_mode(size)
@@ -38,10 +39,61 @@ def mainscreen():
         TextSurf, TextRec = text_objects('Synthopia: Bastion of Scrap', largeText)
         TextRec.center = (width/2, ((height/2) - 100))
         gameDisplay.blit(TextSurf, TextRec)
-        pygame.draw.rect(gameDisplay, grey, (((width/2)-50), height/2,100,50))
+
         #pygame.draw.rect(gameDisplay, grey, ((width - 150), 25, 100, 50)) for in game settings
-        pygame.draw.rect(gameDisplay, grey, (((width / 2) - 200), height / 2, 100, 50))
-        pygame.draw.rect(gameDisplay, grey, (((width / 2) + 100), height / 2, 100, 50))
+
+
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if ((width/2) + 50) > mouse[0] > (width/2)-50 and ((height / 2) + 100) > mouse[1] > (height / 2):
+            pygame.draw.rect(gameDisplay, lightblue, (((width / 2) - 50), height / 2, 100, 50))
+            if click[0] == 1:
+                settings()
+        else:
+            pygame.draw.rect(gameDisplay, grey, (((width / 2) - 50), height / 2, 100, 50))
+
+        if ((width / 2) - 100) > mouse[0] > (width / 2) - 200 and (height/2 + 100) > mouse[1] > (height/2):
+            pygame.draw.rect(gameDisplay, lightblue, (((width / 2) - 200), height / 2, 100, 50))
+            if click[0] == 1:
+                gameloop()
+        else:
+            pygame.draw.rect(gameDisplay, grey, (((width / 2) - 200), height / 2, 100, 50))
+
+        if ((width / 2) + 200) > mouse[0] > (width / 2) + 100 and (height/2)+100 > mouse[1] > (height/2):
+            pygame.draw.rect(gameDisplay, lightblue, (((width / 2) + 100), height / 2, 100, 50))
+            if click[0] == 1:
+                pygame.quit()
+                quit()
+        else:
+            pygame.draw.rect(gameDisplay, grey, (((width / 2) + 100), height / 2, 100, 50))
+
+
+        smallText = pygame.font.Font('freesansbold.ttf', 20)
+        textSurf, textRec = text_objects("Play", smallText)
+        textRec.center = ((width / 2) - 150, height/2 + 25)
+        gameDisplay.blit(textSurf, textRec)
+        textSurf, textRec = text_objects("Settings", smallText)
+        textRec.center = ((width / 2) , height / 2 + 25)
+        gameDisplay.blit(textSurf, textRec)
+        textSurf, textRec = text_objects("Exit", smallText)
+        textRec.center = ((width / 2) + 150, height / 2 + 25)
+        gameDisplay.blit(textSurf, textRec)
+
+
+        pygame.display.update()
+
+
+def settings():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
         pygame.display.update()
 
 
